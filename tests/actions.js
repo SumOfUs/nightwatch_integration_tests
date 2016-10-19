f3b3db846e2d503be5fe17e7e21a28fc0a137d9e
Omar Sahyoun <osahyoun@gmail.com>
2016-10-19 20:00:48 +0100

    Update test script

--- tests/actions.js
+++ tests/actions.js
@@ -14,8 +14,19 @@ module.exports = {
       .assert.containsText('div.item', 'American Samoa')
       .click('button.action-form__submit-button')
       .waitForElementVisible('.thank-you__thanks', 2000)
+
+
+    // Test a french language page
+      .url('https://actions.sumofus.org/a/segolene-royal-defendez-les-abeilles-contre-les-lobbies-de-l-industrie-chimique')
+      .waitForElementVisible('button.action-form__submit-button', 1000)
+      .assert.containsText('.action-form__welcome-name', 'Omar')
+      .click('button.action-form__submit-button')
+      .waitForElementVisible('.two-step__question', 2000)
+      .assert.containsText('.two-step__question', 'partager maintenant la campagne')
+      .click('.two-step__accept')
       .assert.elementPresent('a[type=f]')
       .assert.elementPresent('a[type=t]')
       .end()
+
   }
 }
